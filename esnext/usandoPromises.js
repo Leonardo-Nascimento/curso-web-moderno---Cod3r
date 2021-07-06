142cc99b57e6ba66dc0df2d4f6a58bfdcfbd0795
Leonardo Nascimento <leonardo.nascimento@fitbank.com.br>
2021-07-06 14:44:03 -0300

    async await + uso de promises

--- esnext/usandoPromises.js
+++ esnext/usandoPromises.js
@@ -1,7 +1,6 @@
 //Usando promises
 
 const http = require('http');
-const { reject, concat } = require('lodash');
 
 const getTurma = (letra) => {
     const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`;
