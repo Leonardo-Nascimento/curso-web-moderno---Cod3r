70b64e08166e71646aa2a9d8855c18144a85236b
Leonardo-Nascimento <leonardot.i@hotmail.com>
2021-10-31 13:49:24 -0300

    classe e state 02

--- react/exercicios/src/componentes/Saudacao.jsx
+++ react/exercicios/src/componentes/Saudacao.jsx
@@ -3,9 +3,20 @@ import React, { Component } from "react";
 
 export default class Saudacao extends Component {
 
-    state ={
-        tipo: this.props.tipo,
-        nome: this.props.nome
+    
+    constructor(props){
+        super()
+        
+        
+        //dessa forma estou dizendo que a referencia aponta para a instancia dessa classe (this = saudacao)
+        //ou seja estou chmando uma função que está dentro dessa classe
+        this.setTipo = this.setTipo.bind(this)   
+          
+        
+        this.state ={
+            tipo: props.tipo,
+            nome: props.nome
+        }
     }
 
     setTipo(e){
@@ -31,8 +42,8 @@ export default class Saudacao extends Component {
                 <h1>{tipo} {nome}!</h1>
                 <hr/>
 
-                <input type='text' placeholder="Tipo...." value={tipo} onChange={e => this.setTipo(e)}/>
-                <input type='text' placeholder="Nomw...." value={nome} onChange={e => this.setNome(e)}/>
+                <input type='text' placeholder="Tipo...." value={tipo} onChange={this.setTipo}/>
+                <input type='text' placeholder="Nome...." value={nome} onChange={e=> this.setNome(e)}/>
             </div>
 
 
