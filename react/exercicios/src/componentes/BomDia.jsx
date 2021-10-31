9fecf746f2e0921b60a8f8f027346458c5c7a319
Leonardo-Nascimento <leonardot.i@hotmail.com>
2021-10-31 12:25:29 -0300

    importar multiplos componentes react

---
+++ react/exercicios/src/componentes/BomDia.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigation } from 'react-minimal-side-navigation';
+import { useHistory, useLocation } from "react-router-dom";
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import { faCalendar, faCog, faFile, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Bomdia = () => <h1>Bom Dia</h1>
+
+
+export default Bomdia
---
+++ react/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,4 @@
+import React from "react"
+
+export default () => <h2>Teste2</h2>
+
